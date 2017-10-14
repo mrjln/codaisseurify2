76794654824bfea9cf106a334c5e6c932e94318b
mrjln <marjolein_tromp@hotmail.com>
2017-10-14 15:53:29 +0200

    Migration Add Artist to Songs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171014111124) do
+ActiveRecord::Schema.define(version: 20171014134302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 20171014111124) do
     t.string "album"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "artist_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
+  add_foreign_key "songs", "artists"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,29 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+
+
+#seed some events
+artist_1 = Artist.create(
+  name: string,
+  biography: text,
+  hometown: string,)
+
+artist_1 = Artist.create(
+  name: string,
+  biography: text,
+  hometown: string,)
+
+artist_1 = Artist.create(
+ name: string,
+ biography: text,
+ hometown: string,)
+
+artist_1 = Artist.create(
+ name: string,
+ biography: text,
+ hometown: string,)
+
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
