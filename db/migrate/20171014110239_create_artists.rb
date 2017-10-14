943559181ff65f38c108ced03a344a2440a19eb4
mrjln <marjolein_tromp@hotmail.com>
2017-10-14 13:04:30 +0200

    Created Artist Model w\ name biography and hometown

---
+++ db/migrate/20171014110239_create_artists.rb
@@ -0,0 +1,11 @@
+class CreateArtists < ActiveRecord::Migration[5.1]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.text :biography
+      t.string :hometown
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/artists.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :artist do
+    name "MyString"
+    biography "MyText"
+    hometown "MyString"
+  end
+end
