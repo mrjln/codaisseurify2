2d13f98000f1bd121205717b467a2249ec8be5a7
mrjln <marjolein_tromp@hotmail.com>
2017-10-14 19:08:09 +0200

    Artist contoller update

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,4 @@
 class ArtistsController < ApplicationController
-#  before_action :set_artist
 
   def show; end
 
@@ -7,15 +6,47 @@ class ArtistsController < ApplicationController
      @artist = Artist.all
   end
 
+  private
+
+  def image_params
+  params[:images].present? ? params.require(:images) : []
+  end
+
+end
 
 
-  #def set_artist
-  #  @artist = Artist.find(params[:id])
-  #end
+
+
+
+#  def set_artist
+#    @artist = Artist.find(params[:id])
+#  end
 
 #  def artist_params
 #    params
-#    .permit(:name, :biography, :hometown)
+#   .permit(:name, :biography, :hometown, :images [])
 #  end
 
-end
+#  def image_params
+#    params[:images].present? ? params.require(:images) : []
+#  end
+
+
+
+
+#def set_event
+#  @event = Event.find(params[:id])
+#end
+
+#def event_params
+#  params
+#    .require(:event)
+#    .permit(
+#         :name,:description, :location, :price, :capacity, :includes_food,
+#         :includes_drinks, :starts_at, :ends_at, :active, theme_ids: []
+#    )
+    #theme_ids is and array because an event can have many themes
+#end
+
+
+ #This symbol is called :images because it is defined in the photos.rb. via the ImageUploader
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,4 +3,7 @@ class Artist < ApplicationRecord
   has_many :songs, dependent: :destroy
   has_many :photos, dependent: :destroy
 
+def photos
+  @photos = artist.photos
+end
 end
