59a37d771fa822312383b53ee802ac4472b34e5f
mrjln <marjolein_tromp@hotmail.com>
2017-10-14 13:12:36 +0200

    Create Song model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171014110239) do
+ActiveRecord::Schema.define(version: 20171014111124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20171014110239) do
     t.string "name"
     t.text "biography"
     t.string "hometown"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "titel"
+    t.integer "year"
+    t.string "album"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/factories/songs.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :song do
+    titel "MyString"
+    year 1
+    album "MyString"
+  end
+end
