4e4f43f1ed7c5e481e88d6746ce7b164284af28b
mrjln <marjolein_tromp@hotmail.com>
2017-10-14 17:09:01 +0200

    Created Artist Index

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,11 +1,25 @@
 class ArtistsController < ApplicationController
+  #before_action :set_artist
+
   def show
     @artist = Artist.find(params[:id])
   end
 end
 
+def index
+   @artist = Artist.find(params[:id])
+ end
+
+ def create
+    @artist = Artist.find(params[:id])
+  end
+
 
 private
+
+#def set_artist
+#   @artist = artist.find(params[:id])
+ #end
 
  def artist_params
    params
