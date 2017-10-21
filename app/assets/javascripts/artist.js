5ae5d0974ddfefe50739ddc2aa627f51349acb76
mrjln <marjolein_tromp@hotmail.com>
2017-10-21 10:58:12 +0200

    Add song js

--- app/assets/javascripts/artist.js
+++ app/assets/javascripts/artist.js
@@ -1,15 +1,36 @@
 
-
 function submitSong(event) {
   event.preventDefault();
 
   var title = $("#new-song").val();
-//@marjolein je hebt een span gecreeerd rondom je form input met id = "new-song"
+//@marjolein je hebt een span gecreeerd rondom je form-input met id = "new-song"
 
   createSong(title);
 
   $("#new-todo").val(null);
 }
+
+function createSong(title) {
+
+  var listItem = $("<li></li>");
+  listItem.addClass("song");
+
+  var label = $('<label></label>');
+  label.attr('for',"song-"+ newSongId();
+  label.html(title);
+
+  listItem.append(label);
+
+  $("#songlist").append( listItem );
+
+}
+
+function newSongId() {
+  return $(".song").length  + 1;
+}
+//je begint hier dus weer met een song id van 2 of 1, niet geheel goed maar het werkt
+
+
 
 
 $(document).ready(function() {
